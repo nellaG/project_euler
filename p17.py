eba69f77f5acd84b32e59371655d28025c1ff58d
Jueun Lee <jueun.lee@ifunfactory.com>
2015-01-15 23:08:21 +0900

    p17 solved

--- p17.py
+++ p17.py
@@ -14,28 +14,48 @@ import os
 
 
 def main():
-  for i in range(1, 11):
-    print i
-  num_to_word(1000)
+  length = 0
+  for i in range(1, 1001):
+    num =(num_to_word_wrapper(i))
+    length = length + len(str(num))
+  print length
 
 
-def num_to_word(number):
+def num_to_word_wrapper(number):
   word = ''
+  k = num_to_word(number, word)
+  return k
+
+
+def num_to_word(number, word):
 
   if number % 1000 != number:
     digit = digit_to_word(number / 1000)
-    print digit + ' thousand'
+    word = word + digit + 'thousand'
+    return num_to_word(number % 1000, word)
   elif number % 100 != number:
-    pass
+    digit = digit_to_word(number / 100)
+    word = word + digit + 'hundred' + ('and' if number % 100 != 0 else '')
+    return num_to_word(number % 100, word)
   elif number % 10 != number:
-    pass
+    digit = digit_to_word(number / 10)
+    if digit != 'one':
+      word = word + (digit if number >= 100 else '') + print_to_ten(number / 10)
+    else:
+      # eleven, twelve, thirteen, fourteen, ...
+      tenth = digit_to_tenth(str(number % 10))
+      word = word + tenth
+      return word
+    return num_to_word(number % 10, word)
   else:
-    pass
+    digit = digit_to_word(str(number))
+    word = word + digit
+    return word
 
 
 def digit_to_word(digit):
   return {
-      '0': 'zero',
+      '0': '',
       '1': 'one',
       '2': 'two',
       '3': 'three',
@@ -47,5 +67,30 @@ def digit_to_word(digit):
       '9': 'nine'
   }[str(digit)]
 
+def print_to_ten(digit):
+  return {
+      '0': '',
+      '2': 'twenty',
+      '3': 'thirty',
+      '4': 'fourty',
+      '5': 'fifty',
+      '6': 'sixty',
+      '7': 'seventy',
+      '8': 'eighty',
+      '9': 'ninty',
+  }[str(digit)]
 
+def digit_to_tenth(digit):
+  return {
+      '0': 'ten',
+      '1': 'eleven',
+      '2': 'twelve',
+      '3': 'thirteen',
+      '4': 'fourteen',
+      '5': 'fifteen',
+      '6': 'sixteen',
+      '7': 'seventeen',
+      '8': 'eighteen',
+      '9': 'nineteen',
+  }[str(digit)]
 main()
